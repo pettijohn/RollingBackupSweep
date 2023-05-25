2deccde14cf2ca8f8e281bc6c9f49400cb7c2aee
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-05-25 15:31:49 -0700

    Advancing version number in release automation

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,20 @@
+$ErrorActionPreference = 'Stop'
+
 if([String]::IsNullOrEmpty($Args[0])) { Write-Host "Version is required"; exit 1; }
 $version = $Args[0];
+# Trim starting v --> v0.9.9 --> 0.9.9 because csproj version must be specific x.y.m.n format 
+$versionNum = $version.TrimStart('v')
+
+$projPath = get-item "src\RollingBackupSweep.csproj"
+$csproj = [xml] (get-content $projPath)
+$csproj.Project.PropertyGroup.Version = $versionNum
+$csProj.Save($projPath)
+
+wsl -- ./publish.sh $version
+
+dotnet pack -c Release -o .\out\
+# dotnet nuget push .\out\TaskSchedulerEngine.nupkg
+
 
 dotnet publish src -r win-x64   -c Release -o publish && 
     compress-archive -path .\publish\RollingBackupSweep.exe -destinationpath .\publish\RollingBackupSweep-${version}-win-x64.zip
@@ -7,7 +22,7 @@ dotnet publish src -r win-x64   -c Release -o publish &&
 dotnet publish src -r win-arm64 -c Release -o publish && 
     compress-archive -path .\publish\RollingBackupSweep.exe -destinationpath .\publish\RollingBackupSweep-${version}-win-arm64.zip
 
-$yesno = Read-Host -Prompt "Did you build linux first? Tag git, push all; create ${version} at github and upload? [Y/N]"
+$yesno = Read-Host -Prompt "Tag git, push all, push tags; create ${version} at github and upload? [Y/N]"
 if ($yesno -ieq 'y') {
     git tag $version
     git push --all
