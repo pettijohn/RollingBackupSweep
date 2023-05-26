3924af6397ffa86abc8efe754c2b795d143c9571
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-05-26 09:37:21 -0700

    Publish script tweak

--- publish.ps1
+++ publish.ps1
@@ -4,10 +4,6 @@ $ErrorActionPreference = 'Stop'
 if([String]::IsNullOrEmpty($Args[0])) { Write-Host "Version is required"; exit 1; }
 $version = $Args[0];
 
-# Ensure git committed (we will tag and push on release)
-$changes = $(git status --porcelain)
-if(![String]::IsNullOrEmpty($changes)) { Write-Host "Uncommitted changes in git"; exit 1; }
-
 # Update csproj version number
 # Trim starting v --> v0.9.9 --> 0.9.9 because csproj version must be specific x.y.m.n format 
 $versionNum = $version.TrimStart('v')
@@ -15,6 +11,11 @@ $projPath = get-item "src\RollingBackupSweep.csproj"
 $csproj = [xml] (get-content $projPath)
 $csproj.Project.PropertyGroup.Version = $versionNum
 $csProj.Save($projPath)
+
+# Ensure git committed (we will tag and push on release)
+# Do after modifying csproj version to catch that update
+$changes = $(git status --porcelain)
+if(![String]::IsNullOrEmpty($changes)) { Write-Host "Uncommitted changes in git"; exit 1; }
 
 # Invoke the linux build - cwd is transformed by wsl.exe and passes through
 wsl -- ./publish.sh $version
